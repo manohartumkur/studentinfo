61b6c9b892c8a03a1588aea2c27d390fdb6f2270
Manohara <manohara.ph@finonyx.com>
2021-11-27 20:20:51 +0530

    Second Commit

--- com.fin.gir.first/src/com/fin/git/first/GitFirst.java
+++ com.fin.gir.first/src/com/fin/git/first/GitFirst.java
@@ -3,7 +3,7 @@ package com.fin.git.first;
 public class GitFirst {
 
 	public static void main(String[] args) {
-		System.out.println("Git is Fun");
+		System.out.println("Git is Fun second commit");
 
 	}
 
