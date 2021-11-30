cf86a20dd8ad67cf5dbc3aab3bbd9bcdde32184c
Manohara <manohara.ph@finonyx.com>
2021-11-30 08:23:09 +0530

    Controller returning Bean

---
+++ SpringBootRest/src/main/java/com/finonyx/app/controller/HelloController.java
@@ -0,0 +1,27 @@
+package com.finonyx.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.finonyx.app.model.HelloWorld;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping(value = "/v1/greet")
+	public String greet() {
+		
+		return "Hello World";
+		
+		
+	}
+	
+	@GetMapping(value = "/v1/greetbean")
+	public HelloWorld greetBean() {
+		
+		return new HelloWorld("Hello World");
+		
+		
+	}
+}
---
+++ SpringBootRest/src/main/java/com/finonyx/app/model/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.finonyx.app.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Setter
+@Getter
+@AllArgsConstructor
+public class HelloWorld {
+
+	private String message;
+}
