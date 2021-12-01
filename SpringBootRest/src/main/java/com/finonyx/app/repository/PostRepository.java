d43fb4fd02120f99777d340a2abbd445aef8f4f7
Manohara <manohara.ph@finonyx.com>
2021-12-01 12:30:48 +0530

    post repository

--- SpringBootRest/src/main/java/com/finonyx/app/repository/PostRepository.java
+++ SpringBootRest/src/main/java/com/finonyx/app/repository/PostRepository.java
@@ -1,9 +1,10 @@
 package com.finonyx.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.finonyx.app.model.Post;
-
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
 }
